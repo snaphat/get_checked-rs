d11dcac425b45a621536afa0a75f805387359a19
Aaron Landwehr <snaphat@gmail.com>
2021-08-27 22:00:47 -0400

    - Remove std dependencies - Remove thiserror - Reimplement error handling

--- src/error.rs
+++ src/error.rs
@@ -1,19 +1,35 @@
-use thiserror::Error;
+use core::fmt;
+use write as w;
 
-#[derive(Error, Debug)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub enum Error
 {
-    #[error("index out of bounds: the len is {0} but the index is {1}")]
     IndexError(usize, usize),
-    #[error("slice index starts at {0} but ends at {1}")]
     SliceIndexOrderError(usize, usize),
-    #[error("range start index {0} out of range for slice of length {1}")]
     SliceStartIndexLenError(usize, usize),
-    #[error("range end index {0} out of range for slice of length {1}")]
     SliceEndIndexLenError(usize, usize),
-
-    #[error("attempted to index slice from after maximum usize")]
     StartIndexOverflowError(),
-    #[error("attempted to index slice up to maximum usize")]
     EndIndexOverflowError(),
+}
+
+use Error::{
+    EndIndexOverflowError, IndexError, SliceEndIndexLenError, SliceIndexOrderError,
+    SliceStartIndexLenError, StartIndexOverflowError,
+};
+
+impl fmt::Display for Error
+{
+    #[rustfmt::skip]
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+    {
+        match *self
+        {
+            | IndexError(a, b)              => { w!(f, "index out of bounds: the len is {0} but the index is {1}", a, b) },
+            | SliceIndexOrderError(a, b)    => { w!(f, "slice index starts at {0} but ends at {1}", a, b) },
+            | SliceStartIndexLenError(a, b) => { w!(f, "range start index {0} out of range for slice of length {1}", a, b) },
+            | SliceEndIndexLenError(a, b)   => { w!(f, "range end index {0} out of range for slice of length {1}", a, b) },
+            | StartIndexOverflowError()     => { w!(f, "attempted to index slice from after maximum usize") },
+            | EndIndexOverflowError()       => { w!(f, "attempted to index slice up to maximum usize") },
+        }
+    }
 }
